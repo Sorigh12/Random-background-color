81aa70b6af7fb56c4398d1b4e1bbfe4ef1abe1e6
Sorigh <84138511+Sorigh12@users.noreply.github.com>
2024-03-03 08:15:57 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Accordian from './components/accordione';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordion component  */}
+      <Accordian/>
+    </div>
+  );
+}
+
+export default App;
